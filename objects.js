716775602aa71ab122ea8e388a66f3081502af38
Ariel <cantegone@yahoo.com>
2019-03-27 06:52:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,9 +4,10 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['Phil Ochs'] =("Here's to the State of Mississippi");
+  playlist[artistName] =(songTitle);
   return playlist;
 }
+updatePlaylist("Phil Ochs", "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
   
