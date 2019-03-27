4f7ef235b394c077ddf2f9b86b9de9d4ff1be7a1
Ariel <cantegone@yahoo.com>
2019-03-27 06:55:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,5 +11,6 @@ updatePlaylist("Phil Ochs", "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
+  return playlist;
 }
 removeFromPlaylist(playlist, ["Slowdive"]);
